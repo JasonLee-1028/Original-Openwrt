220de9943cabf797df14d5269308ec950eb49dd2
u know what <74245552+JasonLee-1028@users.noreply.github.com>
2023-03-12 18:28:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 git pull
-git checkout v21.02.5
+git checkout v22.03.3
 
 pushd feeds/packages/lang
 rm -rf golang && svn co https://github.com/openwrt/packages/branches/openwrt-22.03/lang/golang
